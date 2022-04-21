e8b88a4a36c9beae455409c78981179c368c9822
Nikita <nikitax160@yandex.ru>
2022-04-22 01:48:16 +0300

    JRTB-3: added new commands

--- src/main/java/com/github/nikita160/jrtb/bot/JavaRushTelegramBot.java
+++ src/main/java/com/github/nikita160/jrtb/bot/JavaRushTelegramBot.java
@@ -2,6 +2,11 @@ package com.github.nikita160.jrtb.bot;
 
 
 
+import com.github.nikita160.jrtb.command.CommandContainer;
+import com.github.nikita160.jrtb.command.CommandName;
+import com.github.nikita160.jrtb.command.NoCommand;
+import com.github.nikita160.jrtb.service.SendBotMessageImpl;
+import com.github.nikita160.jrtb.service.SendBotMessageService;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
@@ -13,29 +18,30 @@ import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 @Component
 public class JavaRushTelegramBot extends TelegramLongPollingBot {
 
+    public static String COMMAND_PREFIX = "/";
+
     @Value("${bot.username}")
     private String username;
 
     @Value("${bot.token}")
     private String token;
 
+    private final CommandContainer commandContainer;
+
+    public JavaRushTelegramBot(){
+        this.commandContainer = new CommandContainer(new SendBotMessageImpl(this));
+    }
+
     @Override
     public void onUpdateReceived(Update update) {
         if(update.hasMessage() && update.getMessage().hasText()) {
             String message = update.getMessage().getText().trim();
-            String chatId = update.getMessage().getChatId().toString();
-            String from = update.getMessage().getFrom().getFirstName().toString();
-
-            SendMessage sm = new SendMessage();
-            sm.setChatId(chatId);
-            sm.setText(message);
-
-            try {
-                execute(sm);
-            } catch (TelegramApiException e) {
-                //todo add logging to the project.
-                e.printStackTrace();
+            if (message.startsWith(COMMAND_PREFIX)){
+                String commandIdentifier = message.split(" ")[0].toLowerCase();
+                commandContainer.retrieveCommand(commandIdentifier).execute(update);
             }
+            else commandContainer.retrieveCommand(CommandName.NO.getCommandName()).execute(update);
+
         }
 
     }
--- src/main/java/com/github/nikita160/jrtb/command/NoCommand.java
+++ src/main/java/com/github/nikita160/jrtb/command/NoCommand.java
@@ -6,7 +6,7 @@ import org.telegram.telegrambots.meta.api.objects.Update;
 public class NoCommand implements Command{
     private final SendBotMessageService sendBotMessageService;
 
-    public static final String NO_MESSAGE = "Я поддерживаю команды, начинающиеся со слеша(/).\n"
+    private static final String NO_MESSAGE = "Я поддерживаю команды, начинающиеся со слеша(/).\n"
             + "Чтобы посмотреть список команд введите /help";
 
     public NoCommand(SendBotMessageService sendBotMessageService) {
